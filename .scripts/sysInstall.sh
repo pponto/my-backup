26100e2e55805b4796c89029af2f1c8d02ace22c
pponto <pedromoreira@gmail.com>
2019-04-29 10:30:02 -0300

    04_29_2019

--- .scripts/sysInstall.sh
+++ .scripts/sysInstall.sh
@@ -1,35 +1,82 @@
-#!/bin.bash
+#!/bin/sh
 
 # Author: Pedro Moreira
 # Purpose: My script for recover system after a clean install
-# Date Created: 9/6/2018
-# Readme: Pls run in SUpercow mode
-# Date Modified: 9/6/2018
+# Date Created: 04/26/2019
+# Date Modified: 04/26/2019
 
 # Variables
 ## Generic
-update=apt-get update
-upgrade=apt-get upgrage
-upgradeSy=apt-get upgrade && apt-get dist-upgrade
-install=apt-get install
-installNR=apt-get install --no-install-recommends
+update=sudo apt-get update
+upgrade=sudo apt-get upgrage
+upgradeSy=sudo apt-get upgrade && sudo apt-get dist-upgrade
+install=sudo apt-get install
+installNR=sudo apt-get install --no-install-recommends
+removePurge=sudo apt remove --purge
 
 ## Programs
-systemCore=openbox lightdm tint2?
+systemCore=xorg xinit
 
-systemSupport=pnmixer? nm-tray? xfce4-terminal?? pcmanfm geany nitrogen rofi dunst compton obconf obmenu htop neofetch libnotify-bin
+systemTwm=i3-wm i3blocks rofi dunst libnotify-bin network-manager
 
-systemExtra=firefox libre-office lm-sensors
+systemSupport=xfce4-terminal thunar geany nitrogen redshift htop neofetch curl lm-sensors pavucontrol scrot xclip arandr
 
-systemDev=git npm nodejs zsh
+systemExtra=ttf-ubuntu-font-family mplayer ristretto xpdf ufw ubuntu-restricted-extras lxappearance xarchiver
 
-##### faltam: polybar lxappearance?
+systemDev=git zsh nodejs
 
+npmInstallGlobal=sudo npm install -g gulp eslint now heroku  
+
+systemPurge=ttf-mscorefonts-installer gnome-*
+
+echo "--------- Instalando... ---------"
+#Install
 $update
 $upgradeSy
-$install xorg -y
-$installRN lightdm-gtk-greeter -y
-$install $systemCore -y
-$installNR $systemSupport -y
-reboot
+$install $systemCore
+$install $systemTwm
+$install $systemSupport
+$install $systemExtra
 
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+$install $systemDev
+
+wget -O- https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
+sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s $(which zsh)
+npm install spaceship-prompt
+
+$npmInstallGlobal
+
+$removePurge $systemPurge
+
+echo "--------- Configurando... ---------"
+# Config
+git config --global user.name pponto
+git config --global user.email pedromoreira@gmail.com
+git config --global core.editor nano
+
+cd
+git clone https://github.com/pponto/my-backup
+mv my-backup/* .
+rm -rf .git
+
+cd ~/.scripts/i3blocks
+chmod +x *
+cd ..
+chmod +x morc_menu volume sysBackup.sh
+cd
+
+sudo mv xorg.conf /etc/X11/
+
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw status
+
+neofetch
+echo "--------- Acabou Miseravi ---------"
